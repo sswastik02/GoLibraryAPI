16cd5449ade268572ab982a8926048779b2ee5ba
sswastik02 <sswastik02@gmail.com>
2022-05-07 17:58:16 +0530

    1. [Add] Storage file for postgres 2. [Add] Imports for driver for postgres

---
+++ storage/postgres.go
@@ -0,0 +1,30 @@
+package storage
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres" // Using Postgres Driver provided by gorm
+	"gorm.io/gorm"
+)
+
+type Config struct{
+	Host		string
+	Port		string
+	User		string
+	Password	string
+	DBName		string
+	SSLMode		string
+}
+
+// Config structure holds the information for the Database
+
+func NewConnection(config *Config)(*gorm.DB, error){
+	// This function will open a new database using the config file provided
+	dsn:= fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Host,config.Port,config.User,config.Password,config.DBName,config.SSLMode,
+	)
+
+	db, err:= gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db,err;
+}
